44c5c8ec5acf388aa4e187f799385ede8b8abef2
Guille-Rubio <guillermorubiog@gmail.com>
2022-05-05 19:35:17 +0200

    ADD debounce search

--- src/components/Main/Card/Card.jsx
+++ src/components/Main/Card/Card.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Card = () => {
+
+const Card = (props) => {
+
+  useEffect(() => {
+
+    console.log("hello")
+    console.log(props.value)
+  }
+  )
+
+  const { name, order } = props.value
+  const image = props.value.sprites.other.dream_world.front_default
+  const type_1 = props.value.types[0]["type"]["name"] || "";
+  const type_2 = props.value.types[1]?props.value.types[1]["type"]["name"]:"";
+
+ 
+
+
   return <div className="card">
-<h2>pokemon name</h2>
-<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/54.svg" alt="Pokemon" />
-<p>pokemon number</p>
-<p>pokemon type 1</p>
-<p>pokemon type 2</p>
-<p></p>
+
+    <h2>{name}</h2>
+    <img src={image} alt="Pokemon" />
+    <p>#{order}</p>
+    <p>type: {type_1}</p>
+    <p>type: {type_2}</p>
+    <button>View detail</button>
 
   </div>;
 };
